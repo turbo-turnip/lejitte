4539fd30c825bea8fca5423600a17c692c90cb24
SoftwareFuse <75766911+SoftwareFuse@users.noreply.github.com>
2021-01-05 12:45:30 -0800

    fix credits page

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const startBtn = document.querySelector(".start");
+const creditsBtn = document.querySelector(".credits");
 
 startBtn.addEventListener("click", () => {
     document.location.href = "quiz.html";
 });
+creditsBtn.addEventListener("click", () => {
+    document.location.href = "credits.html";
+})
 
 
 
